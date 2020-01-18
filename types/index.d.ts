5f12770bb38549d272d0cd733c43eeec87db51ac
magicdawn <magicdawn@qq.com>
2020-01-18 18:29:08 +0800

    feat(dts): add types/index.d.ts & d.ts test

---
+++ types/index.d.ts
@@ -0,0 +1,65 @@
+export type OnCancel = (fn: (...args: any[]) => any) => void
+
+export interface TimeoutError extends Error {
+  timeout: number
+}
+
+/**
+ * R = return
+ * P = parameter
+ *
+ * ptimeout supports max 6 parameters. (ts only)
+ */
+
+declare function ptimeout<R, P1>(
+  fn: (arg1: P1, onCancel?: OnCancel) => R,
+  timeout: number,
+  cancel?: boolean
+): (arg1: P1) => R
+
+declare function ptimeout<R, P1, P2>(
+  fn: (arg1: P1, arg2: P2, onCancel?: OnCancel) => R,
+  timeout: number,
+  cancel?: boolean
+): (arg1: P1, arg2: P2) => R
+
+declare function ptimeout<R, P1, P2, P3>(
+  fn: (arg1: P1, arg2: P2, arg3: P3, onCancel?: OnCancel) => R,
+  timeout: number,
+  cancel?: boolean
+): (arg1: P1, arg2: P2, arg3: P3) => R
+
+declare function ptimeout<R, P1, P2, P3, P4>(
+  fn: (arg1: P1, arg2: P2, arg3: P3, arg4: P4, onCancel?: OnCancel) => R,
+  timeout: number,
+  cancel?: boolean
+): (arg1: P1, arg2: P2, arg3: P3, arg4: P4) => R
+
+declare function ptimeout<R, P1, P2, P3, P4, P5>(
+  fn: (
+    arg1: P1,
+    arg2: P2,
+    arg3: P3,
+    arg4: P4,
+    arg5: P5,
+    onCancel?: OnCancel
+  ) => R,
+  timeout: number,
+  cancel?: boolean
+): (arg1: P1, arg2: P2, arg3: P3, arg4: P4, arg5: P5) => R
+
+declare function ptimeout<R, P1, P2, P3, P4, P5, P6>(
+  fn: (
+    arg1: P1,
+    arg2: P2,
+    arg3: P3,
+    arg4: P4,
+    arg5: P5,
+    arg6: P6,
+    onCancel?: OnCancel
+  ) => R,
+  timeout: number,
+  cancel?: boolean
+): (arg1: P1, arg2: P2, arg3: P3, arg4: P4, arg5: P5, arg6: P6) => R
+
+export default ptimeout
---
+++ types/index.test-d.ts
@@ -0,0 +1,16 @@
+import ptimeout, { OnCancel, TimeoutError } from './' // f is(n: number) => void
+import { expectType } from 'tsd'
+
+function normal(a: number, b: string): Promise<number> {
+  return Promise.resolve(1)
+}
+
+function useCancel(a: number, b: string, onCancel?: OnCancel): Promise<number> {
+  return Promise.resolve(1)
+}
+
+expectType<(a: number, b: string) => Promise<number>>(ptimeout(normal, 10))
+
+expectType<(a: number, b: string) => Promise<number>>(
+  ptimeout(useCancel, 10, true)
+)
